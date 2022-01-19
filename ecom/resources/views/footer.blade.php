15126fab4f2744c7dd780880ef2b9f49978f3de7
Kartikerror! <kartikkachan9586@gmail.com>
2022-01-19 17:20:40 +0530

    login page done

---
+++ ecom/resources/views/footer.blade.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<footer class="page-footer font-small cyan darken-3">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2020 Copyright:
+      <b> E-Comm.com</b>
+    </div>
+    <!-- Copyright -->
+  
+  </footer>
+  <!-- Footer -->
---
+++ ecom/resources/views/login.blade.php
@@ -0,0 +1,21 @@
+@extends('master')
+@section('content')
+<div class="container custom-login">
+    <div class="row">
+        <div class="col-sm-4 mx-auto">
+            <form>
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">Email address</label>
+                  <input type="email" class="form-control"aria-describedby="emailHelp" placeholder="Enter Email">
+                </div>
+                <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">Password</label>
+                  <input type="password" class="form-control" placeholder="Enter Password">
+                </div>
+            
+                <button type="submit" class="btn btn-primary">Login</button>
+              </form>
+        </div>
+    </div>
+</div>
+@endsection
