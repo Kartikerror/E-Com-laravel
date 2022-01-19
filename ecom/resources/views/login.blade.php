4745acd89cb74220328e02d55de650417f154dfa
Kartikerror! <kartikkachan9586@gmail.com>
2022-01-19 18:35:36 +0530

    Login Done

--- ecom/resources/views/login.blade.php
+++ ecom/resources/views/login.blade.php
@@ -3,14 +3,15 @@
 <div class="container custom-login">
     <div class="row">
         <div class="col-sm-4 mx-auto">
-            <form>
+            <form action="login" method="POST">
+                @csrf
                 <div class="mb-3">
                   <label for="exampleInputEmail1" class="form-label">Email address</label>
-                  <input type="email" class="form-control"aria-describedby="emailHelp" placeholder="Enter Email">
+                  <input type="email" class="form-control" name="email" aria-describedby="emailHelp" placeholder="Enter Email">
                 </div>
                 <div class="mb-3">
                   <label for="exampleInputPassword1" class="form-label">Password</label>
-                  <input type="password" class="form-control" placeholder="Enter Password">
+                  <input type="password" class="form-control" name="password" placeholder="Enter Password">
                 </div>
             
                 <button type="submit" class="btn btn-primary">Login</button>
